ec8e3185cbf749408f2461c4b4b9b0f154d120d7
BENAVENTE CARRILLO DE ALBORNOZ <manuel.benavente@isepceu.es>
2025-12-10 20:33:20 +0100

    añadir modulo Biblioteca Alumno

---
+++ extra-addons/biblioteca_alumno/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Biblioteca Alumno",
+    "version": "1.0",
+    "summary": "Gestión de libros para práctica DAM",
+    "description": "Módulo de ejemplo para gestionar libros en Odoo.",
+    "author": "Manuel B",
+    "depends": ["base"],
+    "data": [
+        "views/biblioteca_libro_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
